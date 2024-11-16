d56287828c035778b12d98b1b29544bbe9d9478f
Nirbhay Kumar <nirbhaysawan8757@gmail.com>
2024-11-17 03:48:13 +0530

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Nirbhay kumar" );
+        System.out.println( "Nirbhay kkumar" );
+        System.out.println( "Hello World!" );
     }
 }
